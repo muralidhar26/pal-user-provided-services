65ec68b4934ddd3f633f6afbb3e7e0f6f7d70d59
srirampvvm <plnvbs@gmail.com>
2018-04-26 14:09:28 -0500

    user-provided db

--- src/main/java/io/pivotal/pal/paluserprovidedservices/HomeController.java
+++ src/main/java/io/pivotal/pal/paluserprovidedservices/HomeController.java
@@ -1,14 +1,49 @@
 package io.pivotal.pal.paluserprovidedservices;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class HomeController {
 
+    @Autowired
+    private DataSource dataSource;
+
+    private Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @PostConstruct
+    private void init() {
+        logger.info("DataSource:" + dataSource);
+        System.out.println("DataSource:" + dataSource);
+    }
+
     @GetMapping("/")
     public String home() {
-        return "Hello PALs from Wells Fargo in Des Moines";
+
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        //jdbcTemplate.execute("update cohort set nickname='Murali' where id = 8");
+        //jdbcTemplate.execute("update cohort set nickname='Murthy' where id = 11");
+        List<Map<String, Object>> list = jdbcTemplate.queryForList("select * from cohort where id in (8,11)");
+//serviceCredentials.jdbcUrl("externaldatabase", "user-provided") +
+        return "<br/>Hello PALs from Wells Fargo in Des Moines<br/>" + list;
+    }
+
+
+    @GetMapping("/update")
+    public void update() {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        jdbcTemplate.execute("update cohort set nickname='Murali1' where id = 8");
+        jdbcTemplate.execute("update cohort set nickname='Murthy1' where id = 11");
+
     }
 
 }
--- src/main/java/io/pivotal/pal/paluserprovidedservices/PalUserProvidedServicesApplication.java
+++ src/main/java/io/pivotal/pal/paluserprovidedservices/PalUserProvidedServicesApplication.java
@@ -1,12 +1,40 @@
 package io.pivotal.pal.paluserprovidedservices;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+//@Configuration
+//@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class PalUserProvidedServicesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PalUserProvidedServicesApplication.class, args);
-	}
+    private Logger logger = LoggerFactory.getLogger(PalUserProvidedServicesApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(PalUserProvidedServicesApplication.class, args);
+
+    }
+
+    @Bean
+    DatabaseServiceCredentials serviceCredentials(@Value("${vcap.services}") String vcapServices) {
+        return new DatabaseServiceCredentials(vcapServices);
+    }
+
+    @Bean
+    public DataSource externaldatabase(DatabaseServiceCredentials serviceCredentials) {
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setURL(serviceCredentials.jdbcUrl("externaldatabase", "user-provided"));
+        logger.info("MysqlDataSource:" + dataSource);
+        System.out.println("MysqlDataSource:" + dataSource);
+        return dataSource;
+    }
 }
